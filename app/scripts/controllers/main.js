27f9f107b042564cae009c4f034c74807e8ebb9d
Renato Rossi <renadlerossi@gmail.com>
2017-03-02 17:33:57 +0000

    Set up environment.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name playOnWeatherApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the playOnWeatherApp
+ */
+angular.module('playOnWeatherApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
