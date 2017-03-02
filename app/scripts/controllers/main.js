c580ecff921e22e823f820065d30d22b56bc5f56
Renato Rossi <renadlerossi@gmail.com>
2017-03-02 23:38:50 +0000

    Set API call and clean elements from the view.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,22 @@
  * Controller of the playOnWeatherApp
  */
 angular.module('playOnWeatherApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope, $http) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.$watch('search', function() {
+      fetch();
+    });
+
+    function fetch() {
+      $http.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=" + $scope.search + "&units=metric&cnt=16&APPID=e57b113884f4ac1565de77e3b580388c")
+        .then(function (response) {
+          console.log(response.data);
+        });
+    }
+
   });
