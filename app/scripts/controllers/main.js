6c92cdf188afa9671a19a23be40db1ef7a5ec330
Renato Rossi <renadlerossi@gmail.com>
2017-03-03 01:24:46 +0000

    Insert weather and date variables.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -22,8 +22,11 @@ angular.module('playOnWeatherApp')
     function fetch() {
       $http.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=" + $scope.search + "&units=metric&cnt=16&APPID=e57b113884f4ac1565de77e3b580388c")
         .then(function (response) {
+          $scope.searchReturn = response.data;
           console.log(response.data);
         });
     }
+
+    $scope.actualDate = new Date();
 
   });
