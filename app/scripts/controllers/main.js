680f1ae2424bdf405c447faf9a08db7f7b9ea998
Renato Rossi <renadlerossi@gmail.com>
2017-03-04 14:47:56 +0000

    Organise data fetching and link weekdays and dates to dynamic list.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,18 +15,38 @@ angular.module('playOnWeatherApp')
       'Karma'
     ];
 
-    $scope.$watch('search', function() {
-      fetch();
-    });
+    $scope.actualDate = new Date();
+    $scope.cardsDate = [];
+    $scope.weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-    function fetch() {
-      $http.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=" + $scope.search + "&units=metric&cnt=16&APPID=e57b113884f4ac1565de77e3b580388c")
+
+    $scope.fetch = function() {
+      if ($scope.search) {
+        var cleanSearchWord = $scope.search.name.split(' ').join('');
+        apiCall(cleanSearchWord);
+      } else {
+        apiCall('Dublin, IE');
+      }
+    };
+
+    $scope.fetch();
+
+    function apiCall(query) {
+      $http.get('http://api.openweathermap.org/data/2.5/forecast/daily?q=' + query + '&units=metric&cnt=16&APPID=e57b113884f4ac1565de77e3b580388c')
         .then(function (response) {
           $scope.searchReturn = response.data;
-          console.log(response.data);
+          angular.forEach($scope.searchReturn.list, function (value, index) {
+            var monthDay = $scope.actualDate.getDate() + index;
+            var weekDay = $scope.weekDays[((monthDay + 1) % 7)];
+            var cardDate = weekDay + ' ' + monthDay;
+            $scope.cardsDate.push(cardDate);
+            console.log(monthDay);
+          });
         });
     }
 
-    $scope.actualDate = new Date();
+    $http.get('assets/common-cities.json').then(function (response) {
+        $scope.commonCities = response.data.cities;
+      });
 
   });
