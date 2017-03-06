349961318aad5599c8f1b36a5559c945ab8e31da
Renato Rossi <renadlerossi@gmail.com>
2017-03-06 01:38:43 +0000

    Load api service in the detail view.

--- app/scripts/controllers/detail.js
+++ app/scripts/controllers/detail.js
@@ -8,10 +8,25 @@
  * Controller of the playOnWeatherApp
  */
 angular.module('playOnWeatherApp')
-  .controller('DetailCtrl',  function () {
+  .controller('DetailCtrl',  function ($scope, weatherData) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    // Get current date
+    $scope.actualDate = new Date();
+
+    let oldSearchword = localStorage.getItem('search');
+
+    loadResults(oldSearchword);
+
+    function loadResults(query) {
+      weatherData.apiCall(query).then(function (response) {
+        $scope.searchReturn = response.data;
+      });
+    }
+
+
   });
